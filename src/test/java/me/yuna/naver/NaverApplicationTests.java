f7324af3f74bad65cdeaf8696eaaa4a2c94fec53
2yuna <y.duck10002@gmail.com>
2023-03-26 22:04:12 +0900

    first commit

---
+++ src/test/java/me/yuna/naver/NaverApplicationTests.java
@@ -0,0 +1,13 @@
+package me.yuna.naver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NaverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
